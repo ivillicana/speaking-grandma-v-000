8ba82c038d73858760ce5bf93490b2b7381f3c76
Isaac Villicana <villicana.i@gmail.com>
2018-04-05 16:34:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,11 +6,11 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
   else
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
   end
 end
